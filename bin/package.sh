651965acd73ac2acd805f42c33d80ba1db90f1e7
Ben Schinn <ben.schinn@mx.com>
2019-09-29 18:46:48 -0600

    shell script for zipping and ignoring zip files

---
+++ bin/package.sh
@@ -0,0 +1,13 @@
+zip -9 -r \
+'--exclude=*node_modules*' \
+'--exclude=*.git*' \
+'--exclude=*.re*' \
+'--exclude=*lib*' \
+'--exclude=*.DS_Store*' \
+'--exclude=*.merlin*' \
+'--exclude=*.bsb.lock*' \
+'--exclude=*package.json*' \
+'--exclude=*bin*' \
+'--exclude=*bsconfig.json*' \
+'--exclude=*README.md*' \
+tamd.zip ~/projects/tamd
